3ba0d443f3b78491b51ecd990b6ab0cb2d476d69
Ahamed-Rashad <ahamedrashad09@gmail.com>
2022-09-10 12:16:45 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 //import logo from './logo.svg';
 import './App.css';
 import Footerr from './Component/Footer/Footerr';
+import Navbar from './Component/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages';
+import About from './Pages/about';
+import Services from './Pages/services';
+import Contact from './Pages/contact';
+import SignUp from './Pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <div className="content-wraper">
+      {/* <div className="content-wraper">
         
-      </div>
+      </div> */}
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
       <Footerr/>
     </div>
   );
---
+++ src/Component/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img src={require('../../Resources/Icon/Logo.PNG')} alt='logo' width={'40px'} height={'40px'}/>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/postjob'>Post Job</NavBtnLink>
+        {/* </NavBtn>
+        <NavBtn> */}
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
